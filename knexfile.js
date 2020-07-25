518de5e5f3449f680694d199506bdfbb4b10969a
Brenda Profiro <brenda.santos@tripletech.com.br>
2020-07-25 00:19:15 -0300

    add migration and seed

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,13 @@ module.exports = {
       database: "knex_api",
       user: "postgres",
       password: "1109",
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
     }
   },
 };
