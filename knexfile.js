a41b2d8f93d7be0ad4a68d1dfcec38be05133776
Brenda Profiro <brenda.santos@tripletech.com.br>
2020-07-24 23:59:16 -0300

    add knexfile

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: "knex_api",
+      user: "postgres",
+      password: "1109",
+    }
+  },
+};
---
+++ src/database/index.js
@@ -0,0 +1,5 @@
+const knexFile = require('../../knexfile');
+const knexfile = require('../../knexfile');
+const knex = require('knex')(knexfile['development']);
+
+module.exports = knex;
