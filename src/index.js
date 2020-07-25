f25aae870d283b5ccb50b03dc475da7211d226c5
Brenda Profiro <brenda.santos@tripletech.com.br>
2020-07-25 01:00:50 -0300

    add error not found

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.use((request, response, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
 app.use((error, request, response, next) => {
   response.status(error.status || 500);
   response.json({ error: error.message });
